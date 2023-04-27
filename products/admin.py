ecdac168f4c3a604c41b73c20032e43b8d34a6a4
MuratOfficial <toimetm@gmail.com>
2023-04-27 13:05:12 +0600

    end of the project

--- products/admin.py
+++ products/admin.py
@@ -3,7 +3,20 @@ from django.contrib import admin
 from products.models import ProductCategory, Product, Basket
 
 admin.site.register(ProductCategory)
-admin.site.register(Product)
 admin.site.register(Basket)
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'image', 'description', 'short_description', ('price', 'quantity'), 'category')
+    # readonly_fields = ('')
+    ordering = ('name',)
+    search_fields = ('name',)
+
+class BasketAdminInline(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('product', 'created_timestamp',)
+    extra = 0
+
+
--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 from users.models import User
+from products.admin import BasketAdminInline
 
-admin.site.register(User)
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    inlines = (BasketAdminInline,)
